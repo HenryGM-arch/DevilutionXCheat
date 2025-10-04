9fa18ae7073e6f758756a7d4e340fe3ca5653c63
HenryGM-arch <henrygm1996@gmail.com>
2025-10-04 00:10:55 -0300

    Update MainActivity.java

--- android-project/app/src/main/java/org/diasurgical/devilutionx/MainActivity.java
+++ android-project/app/src/main/java/org/diasurgical/devilutionx/MainActivity.java
@@ -1,14 +1,34 @@
-Dialog cheatDialog = new Dialog(this);
-cheatDialog.setContentView(R.layout.dialog_cheats);
+package org.diasurgical.devilutionx;
 
-Button btnGod = cheatDialog.findViewById(R.id.btnGodMode);
-Button btnGold = cheatDialog.findViewById(R.id.btnGold);
-Button btnStats = cheatDialog.findViewById(R.id.btnStats);
-Button btnSpells = cheatDialog.findViewById(R.id.btnSpells);
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.Button;
 
-btnGod.setOnClickListener(v -> CheatBridge.enableGodMode());
-btnGold.setOnClickListener(v -> CheatBridge.giveGold(999999));
-btnStats.setOnClickListener(v -> CheatBridge.maxStats());
-btnSpells.setOnClickListener(v -> CheatBridge.maxSpells());
+public class MainActivity extends Activity {
 
-cheatDialog.show();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Crear y mostrar tu diálogo de cheats
+        showCheatDialog();
+    }
+
+    private void showCheatDialog() {
+        Dialog cheatDialog = new Dialog(this);
+        cheatDialog.setContentView(R.layout.dialog_cheats);
+
+        Button btnGod = cheatDialog.findViewById(R.id.btnGodMode);
+        Button btnGold = cheatDialog.findViewById(R.id.btnGold);
+        Button btnStats = cheatDialog.findViewById(R.id.btnStats);
+        Button btnSpells = cheatDialog.findViewById(R.id.btnSpells);
+
+        btnGod.setOnClickListener(v -> CheatBridge.enableGodMode());
+        btnGold.setOnClickListener(v -> CheatBridge.giveGold(999999));
+        btnStats.setOnClickListener(v -> CheatBridge.maxStats());
+        btnSpells.setOnClickListener(v -> CheatBridge.maxSpells());
+
+        cheatDialog.show();
+    }
+}
